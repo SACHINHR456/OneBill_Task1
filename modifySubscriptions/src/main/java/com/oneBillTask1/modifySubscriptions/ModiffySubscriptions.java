15598f961115a6c4fc9cb99ba61219d1e00eea6f
SACHINHR456 <sachinhr456@gmail.com>
2021-05-22 20:54:14 +0530

    Adding Task1 file

---
+++ modifySubscriptions/src/main/java/com/oneBillTask1/modifySubscriptions/ModiffySubscriptions.java
@@ -0,0 +1,331 @@
+package com.oneBillTask1.modifySubscriptions;
+
+public class ModiffySubscriptions {
+	
+	private String subscriber_Account_Number;
+	private String order_Number;
+    private String action_Type;
+    private String product_Name;
+    private String price_Plan_Name;
+    private String subscription_Identifier;
+    private String subscription_Quantity;
+    private String activation_Date;
+    private String address_Line1;
+    private String address_Line2;
+    private String city;
+    private String county;
+    private String state;
+    private String zip;
+    private String zipcode_Extension;
+    private String country;
+    private String address_Attribute_Key;
+    private String address_Attribute_Value;
+    private String attribute_Block_Start;
+    private String attribute;
+    private String attribute_Value;
+    private String order_Attribute_Block_Start;
+    private String order_Attribute_Key;
+    private String order_Attribute_Value;
+    private String device_Type_Block_Start;
+    private String device_Type;
+    private String device_Id;
+    
+    public ModiffySubscriptions() {
+    	
+    }
+
+    
+	public String getSubscriber_Account_Number() {
+		return subscriber_Account_Number;
+	}
+
+
+	public void setSubscriber_Account_Number(String subscriber_Account_Number) {
+		this.subscriber_Account_Number = subscriber_Account_Number;
+	}
+
+
+	public String getOrder_Number() {
+		return order_Number;
+	}
+
+
+	public void setOrder_Number(String order_Number) {
+		this.order_Number = order_Number;
+	}
+
+
+	public String getAction_Type() {
+		return action_Type;
+	}
+
+
+	public void setAction_Type(String action_Type) {
+		this.action_Type = action_Type;
+	}
+
+
+	public String getProduct_Name() {
+		return product_Name;
+	}
+
+
+	public void setProduct_Name(String product_Name) {
+		this.product_Name = product_Name;
+	}
+
+
+	public String getPrice_Plan_Name() {
+		return price_Plan_Name;
+	}
+
+
+	public void setPrice_Plan_Name(String price_Plan_Name) {
+		this.price_Plan_Name = price_Plan_Name;
+	}
+
+
+	public String getSubscription_Identifier() {
+		return subscription_Identifier;
+	}
+
+
+	public void setSubscription_Identifier(String subscription_Identifier) {
+		this.subscription_Identifier = subscription_Identifier;
+	}
+
+
+	public String getSubscription_Quantity() {
+		return subscription_Quantity;
+	}
+
+
+	public void setSubscription_Quantity(String subscription_Quantity) {
+		this.subscription_Quantity = subscription_Quantity;
+	}
+
+
+	public String getActivation_Date() {
+		return activation_Date;
+	}
+
+
+	public void setActivation_Date(String activation_Date) {
+		this.activation_Date = activation_Date;
+	}
+
+
+	public String getAddress_Line1() {
+		return address_Line1;
+	}
+
+
+	public void setAddress_Line1(String address_Line1) {
+		this.address_Line1 = address_Line1;
+	}
+
+
+	public String getAddress_Line2() {
+		return address_Line2;
+	}
+
+
+	public void setAddress_Line2(String address_Line2) {
+		this.address_Line2 = address_Line2;
+	}
+
+
+	public String getCity() {
+		return city;
+	}
+
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+
+	public String getCounty() {
+		return county;
+	}
+
+
+	public void setCounty(String county) {
+		this.county = county;
+	}
+
+
+	public String getState() {
+		return state;
+	}
+
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+
+	public String getZip() {
+		return zip;
+	}
+
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+
+	public String getZipcode_Extension() {
+		return zipcode_Extension;
+	}
+
+
+	public void setZipcode_Extension(String zipcode_Extension) {
+		this.zipcode_Extension = zipcode_Extension;
+	}
+
+
+	public String getCountry() {
+		return country;
+	}
+
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+
+	public String getAddress_Attribute_Key() {
+		return address_Attribute_Key;
+	}
+
+
+	public void setAddress_Attribute_Key(String address_Attribute_Key) {
+		this.address_Attribute_Key = address_Attribute_Key;
+	}
+
+
+	public String getAddress_Attribute_Value() {
+		return address_Attribute_Value;
+	}
+
+
+	public void setAddress_Attribute_Value(String address_Attribute_Value) {
+		this.address_Attribute_Value = address_Attribute_Value;
+	}
+
+
+	public String getAttribute_Block_Start() {
+		return attribute_Block_Start;
+	}
+
+
+	public void setAttribute_Block_Start(String attribute_Block_Start) {
+		this.attribute_Block_Start = attribute_Block_Start;
+	}
+
+
+	public String getAttribute() {
+		return attribute;
+	}
+
+
+	public void setAttribute(String attribute) {
+		this.attribute = attribute;
+	}
+
+
+	public String getAttribute_Value() {
+		return attribute_Value;
+	}
+
+
+	public void setAttribute_Value(String attribute_Value) {
+		this.attribute_Value = attribute_Value;
+	}
+
+
+	public String getOrder_Attribute_Block_Start() {
+		return order_Attribute_Block_Start;
+	}
+
+
+	public void setOrder_Attribute_Block_Start(String order_Attribute_Block_Start) {
+		this.order_Attribute_Block_Start = order_Attribute_Block_Start;
+	}
+
+
+	public String getOrder_Attribute_Key() {
+		return order_Attribute_Key;
+	}
+
+
+	public void setOrder_Attribute_Key(String order_Attribute_Key) {
+		this.order_Attribute_Key = order_Attribute_Key;
+	}
+
+
+	public String getOrder_Attribute_Value() {
+		return order_Attribute_Value;
+	}
+
+
+	public void setOrder_Attribute_Value(String order_Attribute_Value) {
+		this.order_Attribute_Value = order_Attribute_Value;
+	}
+
+
+	public String getDevice_Type_Block_Start() {
+		return device_Type_Block_Start;
+	}
+
+
+	public void setDevice_Type_Block_Start(String device_Type_Block_Start) {
+		this.device_Type_Block_Start = device_Type_Block_Start;
+	}
+
+
+	public String getDevice_Type() {
+		return device_Type;
+	}
+
+
+	public void setDevice_Type(String device_Type) {
+		this.device_Type = device_Type;
+	}
+
+
+	public String getDevice_Id() {
+		return device_Id;
+	}
+
+
+	public void setDevice_Id(String device_Id) {
+		this.device_Id = device_Id;
+	}
+
+
+	@Override
+	public String toString() {
+		return "ModiffySubscriptions [subscriber_Account_Number=" + subscriber_Account_Number + ", order_Number="
+				+ order_Number + ", action_Type=" + action_Type + ", product_Name=" + product_Name
+				+ ", price_Plan_Name=" + price_Plan_Name + ", subscription_Identifier=" + subscription_Identifier
+				+ ", subscription_Quantity=" + subscription_Quantity + ", activation_Date=" + activation_Date
+				+ ", address_Line1=" + address_Line1 + ", address_Line2=" + address_Line2 + ", city=" + city
+				+ ", county=" + county + ", state=" + state + ", zip=" + zip + ", zipcode_Extension="
+				+ zipcode_Extension + ", country=" + country + ", address_Attribute_Key=" + address_Attribute_Key
+				+ ", address_Attribute_Value=" + address_Attribute_Value + ", attribute_Block_Start="
+				+ attribute_Block_Start + ", attribute=" + attribute + ", attribute_Value=" + attribute_Value
+				+ ", order_Attribute_Block_Start=" + order_Attribute_Block_Start + ", order_Attribute_Key="
+				+ order_Attribute_Key + ", order_Attribute_Value=" + order_Attribute_Value
+				+ ", device_Type_Block_Start=" + device_Type_Block_Start + ", device_Type=" + device_Type
+				+ ", device_Id=" + device_Id + "]";
+	}
+
+
+
+
+	
+    
+
+}
